f90c8937e425906a9e9be0c933ea2d9335d66b3a
Madhuri2195 <2400032195@kluniversity.in>
2025-11-28 16:59:58 +0530

    Added Navbar component and basic layout

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{background:"#b30000", padding:"15px", color:"white", textAlign:"center"}}>
+      <h2>KL University Student Portal</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
