2fcd3fe070959cf6731643b8e4d79a5f5b2f82ee
Madhuri2195 <2400032195@kluniversity.in>
2025-11-28 17:02:23 +0530

    Added Dashboard page with modules list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Dashboard from "./Components/Dashboard";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 style={{textAlign:"center"}}>Welcome to Student Portal</h1>
+      <Dashboard />
     </>
   );
 }
---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,14 @@
+function Dashboard() {
+  return (
+    <div style={{padding:"20px"}}>
+      <h3>Dashboard</h3>
+      <ul>
+        <li>Attendance</li>
+        <li>Results</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Dashboard;
